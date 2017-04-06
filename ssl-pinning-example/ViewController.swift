2fec1956bee37f0aaf485486be1bddb2b2e801c2
Dmitry Fink <dmitry@finik.net>
2017-04-05 17:05:01 -0700

    Initial Commit

---
+++ ssl-pinning-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ssl-pinning-example
+//
+//  Created by Dmitry Fink on 4/5/17.
+//  Copyright © 2017 Dmitry Fink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
